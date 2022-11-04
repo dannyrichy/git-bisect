432db426f2841b86dfc4d5faa9eeffc70f8b49ed
the-nihilist-ninja <richydany@gmail.com>
2022-11-04 15:30:13 +0100

    Added skeleton for Second method

--- core/algo.py
+++ core/algo.py
@@ -4,7 +4,7 @@ from procrustes import permutation
 from helper import timer_func
 
 
-class Algorithm:
+class Permuter:
     """"
     Parent class for all the methods
     """
@@ -15,7 +15,7 @@ class Algorithm:
         self.model_width = model_width
 
 
-class ActivationMethod(Algorithm):
+class ActivationMethod(Permuter):
     def __init__(self, archi, model_width) -> None:
         super().__init__(archi, model_width)
 
@@ -54,6 +54,7 @@ class ActivationMethod(Algorithm):
         """
         if not (isinstance(model_a_act, numpy.ndarray) and isinstance(model_b_act, numpy.ndarray)):
             raise TypeError("Activations must be numpy array")
+        
         if model_a_act.shape == model_b_act.shape:
             raise Exception(f"Model architecture don't match: model_a has {len(model_a_act)} layers and model_b has {len(model_b_act)}")
         
@@ -64,4 +65,41 @@ class ActivationMethod(Algorithm):
             ]
         
         return self.perm
-                 
+
+    def get_loss(self):
+        """
+        Gets loss value of the operation
+
+        :return: Loss 
+        :rtype: float
+        """
+        return self.loss
+
+class GreedyAlgorithm(Permuter):
+    def __init__(self, archi: list[int], model_width) -> None:
+        """
+        _summary_
+
+        :param archi: _description_
+        :type archi: list[int]
+        :param model_width: _description_
+        :type model_width: _type_
+        """
+        super().__init__(archi, model_width)
+    
+    def get_permutation(self, model_a, model_b):
+        """
+        Generate permutation for the weights of model B
+
+        :param model_a: Model A
+        :type model_a: torch.nn
+        
+        :param model_b: Model B
+        :type model_b: torch.nn
+        
+        :return: List of permutation of each layer
+        :rtype: 
+        """
+    
+        
+        return numpy.zeros((2,2))
