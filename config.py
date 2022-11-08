207e8481714505b5c0f7d31def8a3cc1a87dfe4b
the-nihilist-ninja <richydany@gmail.com>
2022-11-08 12:31:43 +0100

    Merged master

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
+import torch
+
 TIME_FLAG = False
 
-MLP_MODEL1_PATH = Path('stash/mlp_20221105-105343.pth')
-MLP_MODEL2_PATH = Path('stash/mlp_20221105-104646.pth')
+MLP_MODEL1_PATH = Path("stash/mlp_20221105-105343.pth")
+MLP_MODEL2_PATH = Path("stash/mlp_20221105-104646.pth")
+
+DEVICE = torch.device("cuda:0")
